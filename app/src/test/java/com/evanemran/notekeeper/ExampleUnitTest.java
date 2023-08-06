aef19f1d2b17ba1c22204f8d2e4641e22a09d037
Hrhariomverma <vermahariom1000@gmail.com>
2023-08-06 21:14:59 +0530

    this is NoteKeeper project

---
+++ app/src/test/java/com/evanemran/notekeeper/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.evanemran.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+//Nagesh_kumar
+//Project Start date(03/07/2023)
+//Project end date(02/08/2023)
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
